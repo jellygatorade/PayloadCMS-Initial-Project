bf70bc31c97c6edc2d1285a1a32a486ad8287c1d
Kevin Kane <kkane@ncartmuseum.org>
2023-02-01 09:32:22 -0500

    update path to RelationshipProvider component

--- components/ImageCell.js
+++ components/ImageCell.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { useListRelationships } from "/node_modules/payload/dist/admin/components/views/collections/List/RelationshipProvider";
+import { useListRelationships } from "../node_modules/payload/dist/admin/components/views/collections/List/RelationshipProvider";
 
 // Following Discord thread
 // Populate Data In Cell Component
